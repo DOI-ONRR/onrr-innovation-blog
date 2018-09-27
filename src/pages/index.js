876145ffe15e4f7bd35399729e81f449766480ce
brentryanjohnson <brent.johnson@onrr.gov>
2018-09-27 14:26:40 -0700

    Tweaks spacing between posts, adds whitespace

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,10 +25,14 @@ class BlogIndex extends React.Component {
         {posts.map(({ node }) => {
           const title = get(node, 'frontmatter.title') || node.fields.slug
           return (
-            <div key={node.fields.slug}>
+            <div 
+              style={{
+                marginBottom: rhythm(1.5),
+              }}
+              key={node.fields.slug}>
               <h2
                 style={{
-                  marginBottom: rhythm(-0.2),
+                  marginBottom: rhythm(-0.1),
                 }}
               >
                 <Link 
